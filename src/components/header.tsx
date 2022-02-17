74e94ab7de19aec140be87970a7827537665cebb
Oguzhan-Burak Bozkurt <burakbozkurt94@gmail.com>
2022-02-17 20:31:14 +0100

    seo: twitter img, favicon fix

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
             <title>Oguzhan Burak Bozkurt</title>
 
             {/* preloads */}
-            <link rel="icon" href="/public/favicon.ico"/>
+            <link rel="icon" href="/public/icons/favicon.ico"/>
             <link rel="preload"
                   href="/public/fonts/SB/MSBee-Thin.woff2"
                   as="font"
@@ -41,7 +41,7 @@ const Header = () => {
             <meta name="twitter:site" content="@0xBuro"/>
             <meta name="twitter:title" content="Oguzhan Burak Bozkurt, Software Engineer"/>
             <meta name="twitter:description" content="Developer Portfolio of @0xBuro"/>
-            <meta name="twitter:image" content="/public/images/portfolio-light.png"/>
+            <meta name="twitter:image" content="/images/portfolio-light.png"/>
 
                 {/* Open Graph */}
                 <meta property="og:url" content="https://burakbo.vercel.app" />
@@ -55,13 +55,13 @@ const Header = () => {
                     content="Nextjs + Sass Portfolio | burakbo"/>
                 <meta
                     property="og:image"
-                    content="/public/images/portfolio-light.png"
+                    content="/images/portfolio-light.png"
                 />
 
             {/* link */}
-            <link rel="icon" href="/public/favicon.ico"/>
+            <link rel="icon" href="/public/icons/favicon.ico"/>
             <link rel="canonical" href="https://burakbo.vercel.app"/>
-            <link rel="apple-touch-icon" href="/public/favicon.ico"/>
+            <link rel="apple-touch-icon" href="/public/icons/favicon.ico"/>
         </Head>
     )
 }
